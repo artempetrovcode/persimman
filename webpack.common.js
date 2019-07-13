94f1272f907229fe4fee99fba7b86f5a067be887
Artem Petrov <artempetrov@fb.com>
2019-07-13 10:57:07 -0700

    copy fav

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: ['whatwg-fetch', path.resolve(__dirname, 'src', 'index.js')],
@@ -18,8 +19,16 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'docs'),
   },
-  plugins: [new HtmlWebpackPlugin({
+  plugins: [
+    new CopyPlugin([
+      { 
+        from: path.resolve(__dirname, 'public/favicon.ico'),
+        to: path.resolve(__dirname, 'docs/favicon.ico'),
+      },
+    ]),
+    new HtmlWebpackPlugin({
       filename: path.resolve(__dirname, 'docs/index.html'),
       template: path.resolve(__dirname, 'src/index.html'),
-  })]
+    })
+  ],
 };
