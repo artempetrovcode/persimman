a3da55dbd6be07279b02460e02ca2c4362e4fdbe
Artem Petrov <artempetrov@fb.com>
2019-07-13 10:21:49 -0700

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'docs'),
+  },
   entry: ['whatwg-fetch', path.resolve(__dirname, 'src', 'index.js')],
   module: {
     rules: [
