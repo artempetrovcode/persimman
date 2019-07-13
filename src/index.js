0fcea0429d88ec8417d0a5d7bcf9df7b8b1a45d4
Artem Petrov <artempetrov@fb.com>
2019-07-13 11:00:27 -0700

    fix scripts

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 
-function x(s: string): number {
-    return 1;
-}
-
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
