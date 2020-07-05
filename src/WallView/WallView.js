3002d903902b0ba516a1dd50cf30b23d4f1ced29
Artem Petrov <artempetrov@fb.com>
2020-07-05 16:59:58 -0700

    add time to the wall

--- src/WallView/WallView.js
+++ src/WallView/WallView.js
@@ -3,6 +3,7 @@ import type {Todo} from '../Todo';
 import * as React from 'react';
 import {getDateForThisZone, getDayTimestampForThisZone2} from '../lib/timeUtils';
 import StateContext from '../StateContext';
+import TodoDateTimeInput from '../TodoListView/TodoDateTimeInput';
 
 const {useContext} = React;
 
@@ -25,33 +26,61 @@ function WallView() {
 
   const sortedDayTimestamps = Object.keys(groupedByDate).map(Number).sort((a,b) => b - a);
 
+  const handleStartChange = () => {
+
+  }
+
+  const handleEndChange = () => {
+    
+  }
+
   return (
-    <table border="1px" style={{borderCollapse: 'collapse'}}>
-      <thead>
-        <tr>
-        {sortedDayTimestamps.map(dayTimestamp => (
-          <th key={dayTimestamp}>
-            {(new Date(Number(dayTimestamp)).toDateString())}
-          </th>
-        ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
+    <>
+      <div>
+        <label>
+          Start
+          <TodoDateTimeInput 
+            onChange={handleStartChange}
+            onCancel={() => {}}
+            timestamp={Date.now()}
+          />
+        </label>
+        <label>
+          End
+          <TodoDateTimeInput 
+            onChange={handleEndChange}
+            onCancel={() => {}}
+            timestamp={Date.now()}
+          />
+        </label>
+      </div>
+      <table border="1px" style={{borderCollapse: 'collapse'}}>
+        <thead>
+          <tr>
           {sortedDayTimestamps.map(dayTimestamp => (
-          <td key={dayTimestamp}>
-            <ul>
-              {groupedByDate[dayTimestamp].map(todo => (
-                <li key={todo.id}>
-                  <s>{todo.text} [{new Date(todo.completedAt).toLocaleTimeString()}]</s> 
-                </li>
-              ))}
-            </ul>
-          </td>
+            <th key={dayTimestamp}>
+              {(new Date(Number(dayTimestamp)).toDateString())}
+            </th>
           ))}
-        </tr>
-      </tbody>
-    </table>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {sortedDayTimestamps.map(dayTimestamp => (
+            <td key={dayTimestamp}>
+              <ul>
+                {groupedByDate[dayTimestamp].map(todo => (
+                  <li key={todo.id}>
+                    <s>{todo.text} [{new Date(todo.completedAt).toLocaleTimeString()}]</s> 
+                  </li>
+                ))}
+              </ul>
+            </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 }
 
